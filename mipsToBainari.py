2418452122c60191482d9ad9926221db8e42d9ed
miguelcumbalaza <miguelcumbalaza1@gmail.com>
2021-09-15 22:45:22 -0500

    lectura y gardado de datos linea por linea para verificacion

--- mipsToBainari.py
+++ mipsToBainari.py
@@ -1,10 +1,25 @@
 from sys import stdin
+import os
 
-def Data():
-    datos = stdin.readline()
-
+def clear():
+    os.system('cls')
+def lectura(matrix):
+    mips_binario = 'mips/mipsToBinary.txt'
+    with open(mips_binario) as f_obj:
+        for line in f_obj:
+            particiones = line.split(" ")
+            matrix.append(particiones)
+    print(matrix)
 def main():
-    Data()
-
-
+    matrix = []
+    opcion = 0
+    clear()
+    print("Bienvenido al conversor de mips a binario\n")
+    while(opcion != 2 and opcion != 1):
+        opcion = int(input("por favor ingrese la opcion que desea realizar:\n1. Convertir codigo mips a codigo binario\n2. Cerrar\n:"))
+        clear()
+        if(opcion==1):
+            lectura(matrix)
+            opcion = 0
+    clear()
 main()
